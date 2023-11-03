d056596542d6675d6ba4e1e19a88114e5642a1ee
osawa-koki <koki@osawa.dev>
2023-11-03 17:21:13 +0900

    `img` -> `Next/Image`

--- www/pages/museum.tsx
+++ www/pages/museum.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 import fs from 'fs'
 import { Splide, SplideSlide } from '@splidejs/react-splide'
 import setting from '../setting'
@@ -20,12 +21,12 @@ export default function Home (props: Props): React.JSX.Element {
           }}
           className='d-block m-auto'
           style={{
-            width: setting.imageSize
+            width: `${setting.imageSize}px`
           }}
         >
           {imageFilePaths.map((imageFilePath) => (
-            <SplideSlide key={imageFilePath} className='d-block position-relative w-100 ratio ratio-1x1' style={{ maxWidth: setting.imageSize }}>
-                <img className="d-block position-absolute top-0 start-0 bottom-0 end-0" src={`/images/${imageFilePath}`} alt={`fractal (${imageFilePath})`} />
+            <SplideSlide key={imageFilePath} className='d-block position-relative w-100 ratio ratio-1x1' style={{ maxWidth: `${setting.imageSize}px` }}>
+                <Image width={setting.imageSize} height={setting.imageSize} className="d-block position-absolute top-0 start-0 bottom-0 end-0" src={`/images/${imageFilePath}`} alt={`fractal (${imageFilePath})`} />
                 <div className='position-absolute d-flex justify-content-start align-items-center ps-2' style={{ height: '2rem', fontFamily: 'Consolas' }}>{imageFilePath}</div>
               </SplideSlide>
           ))}
--- www/setting.ts
+++ www/setting.ts
@@ -1,12 +1,6 @@
-import Env from './next.config.js'
-const isProd = process.env.NODE_ENV === 'production'
-
 const setting = {
-  isProd: Env.isProd,
-  basePath: Env.basePath,
-  apiPath: isProd ? '' : 'http://localhost:8000',
   title: '🌶️ Fractal Museum 🌶️',
-  imageSize: '512px'
+  imageSize: 512
 }
 
 export default setting
